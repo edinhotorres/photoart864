88feefb3eab06a780a394b3727021200e0ebae69
edinhotorres <edinhotorres@gmail.com>
2020-03-31 01:08:31 +0000

    nova galeria + updates na área Contato + área sobre + efeitos fadein

--- js/js-photoart/update.js
+++ js/js-photoart/update.js
@@ -10,6 +10,9 @@ const familiaDescription = "Viva em família e eternize os mais lindos momentos!
 const festasAniversarioDescription = "O tempo voa! Existem datas especiais que precisam de registros para futuras recordações!";
 const femininoDescription = "Força, alegria e expressão de feminilidade!";
 
+/*Funcionamento de mecânica do FadeIn*/
+let i=0;
+
 function update(){
     alteraDescricoes();
 }
@@ -25,26 +28,60 @@ function alteraDescricoes(){
 
 function updateInfantil(){
     document.querySelector('.infantil-description').innerHTML=infantilDescription;
+    fadeElementIn(document.querySelector('.fadein'));
     // document.querySelector('#viewer').style.backgroundImage = 'url(../../strg/00-45-78-c5-68-ii/ei-imghd-1.jpg)';
 }
 
 function updateCasal(){
     document.querySelector('.casal-description').innerHTML=casalDescription;
+    fadeElementIn(document.querySelector('.fadein'));
     // document.querySelector('#viewer').style.backgroundImage = 'url(../../strg/00-4B-7D-E2-61-ec/ec-imghd-1.jpg)';
 }
 
 function updateFeminino(){
     document.querySelector('.feminino-description').innerHTML=femininoDescription;
+    fadeElementIn(document.querySelector('.fadein'));
     // document.querySelector('#viewer').style.backgroundImage = 'url(../../strg/00-2C-7A-E1-E3-ef/ef-imghd-1.jpg)';
 }
 
 function updateGestante(){
     document.querySelector('.gestante-description').innerHTML=gestanteDescription;
+    fadeElementIn(document.querySelector('.fadein'));
     // document.querySelector('#viewer').style.backgroundImage = 'url(../../strg/00-2C-1A-9B-3B-eg/eg-imghd-2.jpg)';
 }
 
 function updateFestasAniversario(){
     document.querySelector('.festas-aniversario-description').innerHTML=festasAniversarioDescription;
+    fadeElementIn(document.querySelector('.fadein'));
+    
     // document.querySelector('#viewer').style.backgroundImage = 'url(../../strg/00-2C-7A-E1-E3-ef/ef-imghd-1.jpg)';
+}
+function updateFamilia(){
+    document.querySelector('.familia-description').innerHTML=familiaDescription;
+    fadeElementIn(document.querySelector('.fadein'));
+    // document.querySelector('#viewer').style.backgroundImage = 'url(../../strg/00-2C-7A-E1-E3-ef/ef-imghd-1.jpg)';
+}
+
+function updateSobre(){
+    fadeElementIn(document.querySelector('.about-pic'));
+}
+
+function updateContato(){
+    fadeElementIn(document.querySelector('.text-msg-box1'));
+    fadeElementIn(document.querySelector('.text-msg-box2'));
+    fadeElementIn(document.querySelector('.text-msg-box3'));
+}
+
+function updateEnsaios(){
+    fadeElementIn(document.querySelector('#content'));
+}
+
+// exibe o elemento suavemente
+function fadeElementIn(elemento) {
+    if (i < 101){ // enquanto i for menor que 100, faça:
+        elemento.style.opacity = i+'%'; // opacidade = i,
+        i++; // aumente 1 no i;
+        setTimeout(() => fadeElementIn(elemento), 8); // chame esta funcao novamente em 40 milissegundos
+    }
 }
 
